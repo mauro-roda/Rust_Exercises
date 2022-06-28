24d81f0eee53b68b1286bee468b2be215d8f1530
Mauro <mauro.roda@hotmail.com>
2022-06-28 16:45:34 +0100

    clean up files

--- ex_1/target/rls/debug/deps/ex_1-14056025cf04a7e2.d
+++
@@ -1,5 +0,0 @@
-d:\.projects\Rust\Rust_Exercises\ex_1\target\rls\debug\deps\ex_1-14056025cf04a7e2.rmeta: src\main.rs
-
-d:\.projects\Rust\Rust_Exercises\ex_1\target\rls\debug\deps\ex_1-14056025cf04a7e2.d: src\main.rs
-
-src\main.rs:
--- ex_1/target/rls/debug/deps/ex_1-c3a274112e8578c7.d
+++
@@ -1,5 +0,0 @@
-d:\.projects\Rust\Rust_Exercises\ex_1\target\rls\debug\deps\ex_1-c3a274112e8578c7.rmeta: src\main.rs
-
-d:\.projects\Rust\Rust_Exercises\ex_1\target\rls\debug\deps\ex_1-c3a274112e8578c7.d: src\main.rs
-
-src\main.rs:
